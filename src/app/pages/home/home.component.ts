b29c796c3edca4e3ba9bac4107c681cf5b855184
Tamta <tamtakopadz16@gmail.com>
2024-11-05 20:42:39 +0400

    update home text

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  text: string = 'Angular Developer'; // Text to display
+  displayedText: string = '';          // Text that will be shown one character at a time
+  typingSpeed: number = 100;           // Adjust speed (in ms) for typing
+
+  ngOnInit() {
+    this.startTyping();
+  }
+
+  startTyping() {
+    let index = 0;
+    const interval = setInterval(() => {
+      if (index < this.text.length) {
+        this.displayedText += this.text[index];
+        index++;
+      } else {
+        clearInterval(interval); // Stop typing once text is complete
+      }
+    }, this.typingSpeed);
+  }
+  
 
 }
