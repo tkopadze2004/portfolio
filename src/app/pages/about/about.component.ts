01c32d3de928afc438c01ceca162bdd369ebee12
Tamta <tamtakopadz16@gmail.com>
2024-11-25 18:10:12 +0400

    add skills

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,13 +1,13 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { SkillItemComponent } from "../../skill-item/skill-item.component";
+import { SkillItemComponent } from '../../skill-item/skill-item.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [NgFor, SkillItemComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 export class AboutComponent {
   skills = [
@@ -16,41 +16,48 @@ export class AboutComponent {
       icon: 'skills/angular.png',
     },
     {
+      name: 'JavaScript',
+      icon: 'skills/js.png',
+    },
+    {
       name: 'TypeScript',
       icon: 'skills/ts.png',
-    },
-    {
-      name: 'Tailwind CSS',
-      icon: 'skills/tailwind.png',
-
-    },
-    {
-      name: 'npm',
-      icon: 'skills/npm.png',
-    },
-    {
-      name: 'sass',
-      icon: 'skills/sass.png',
-    },
-    {
-      name: 'css',
-      icon: 'skills/css.png',
     },
     {
       name: 'ngrx',
       icon: 'skills/ngrx.png',
     },
     {
+      name: 'rxjs',
+      icon: 'skills/rxjs.png',
+    },
+    {
+      name: 'html',
+      icon: 'skills/html.png',
+    },
+    {
+      name: 'sass',
+      icon: 'skills/sass.png',
+    },
+    {
+      name: 'Tailwind CSS',
+      icon: 'skills/tailwind.png',
+    },
+    {
+      name: 'css',
+      icon: 'skills/css.png',
+    },
+    {
       name: 'Bootstrap',
       icon: 'skills/bootstrap.png',
     },
     {
-      name: 'JavaScript',
-      icon: 'skills/js.png',
-    },
-    {
       name: 'Git',
       icon: 'skills/git.png',
+    },
+    {
+      name: 'npm',
+      icon: 'skills/npm.png',
     },
     {
       name: 'postman',
