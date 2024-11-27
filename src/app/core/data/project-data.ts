cc56342396476025ab49c5cf49f8a7e61fb52e7e
Tamta <tamtakopadz16@gmail.com>
2024-11-27 14:36:30 +0400

    update images

--- src/app/core/data/project-data.ts
+++ src/app/core/data/project-data.ts
@@ -1,23 +1,26 @@
 export const PROJECTS = [
   {
-    name: 'ragac',
-    description: 'description',
-    skills: ['html,css'],
-    link: 'socials/linkedin.png',
-    img: 'socials/linkedin.png',
+    name: 'ecommerce',
+    description:
+      'Developed an Angular-based e-commerDeveloped an Angular-based e-commerce app with optimized product listings and interactive features for an engaging shopping experience. Developed an Angular-based e-commerce app with optimized product listings and interactive features for an engaging shopping experience.ce app with optimized product listings and interactive features for an engaging shopping experience.',
+    skills: ['angular 17typescript,html,scss,Angular material,rxjs'],
+    link: 'https://alte-ecommerce-15c99.web.app/',
+    img: 'ecommerce.png',
   },
   {
-    name: 'ragac',
-    description: 'description',
-    skills: ['html,css'],
-    link: 'socials/linkedin.png',
-    img: 'socials/linkedin.png',
+    name: 'task manager',
+    description:
+      'Built a task management application in Angular that enables users to create projects, assign team members, and organize tasks on a Kanban-style board',
+      skills: ['angular 18, typescript,html,scss,Angular material,rxjs'],
+      link: 'https://task-manager-38a6d.web.app/',
+    img: 'task-manager.png',
   },
   {
-    name: 'ragac',
-    description: 'description',
-    skills: ['html,css'],
-    link: 'socials/linkedin.png',
-    img: 'socials/linkedin.png',
+    name: 'fakeflix',
+    description:
+      'Created a responsive streaming service interface clone in Angular, with real-time data fetching and smooth, user-friendly navigation.',
+      skills: ['angular 18, typescript,html,scss,Angular material,rxjs'],
+    link: 'https://fakeflix-f0041.web.app/',
+    img: 'fakeflix.png',
   },
 ];
