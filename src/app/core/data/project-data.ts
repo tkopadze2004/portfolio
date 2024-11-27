5a3e8b30afac77dfcfc7dc35ee55bae4d008381a
Tamta <tamtakopadz16@gmail.com>
2024-11-27 17:33:30 +0400

    update responsiveness

--- src/app/core/data/project-data.ts
+++ src/app/core/data/project-data.ts
@@ -10,16 +10,16 @@ export const PROJECTS = [
   {
     name: 'task manager',
     description:
-      'Built a task management application in Angular that enables users to create projects, assign team members, and organize tasks on a Kanban-style board',
-      skills: ['angular 18, typescript,html,scss,Angular material,rxjs'],
-      link: 'https://task-manager-38a6d.web.app/',
+      'Built a task management application in Angular that        Developed an Angular-based e-commerDeveloped an Angular-based e-commerce app with optimized product listings and interactive features for an engaging shopping experience. Developed an Angular-based e-commerce app with optimized product listings and interactive features for an engaging shopping experience.ce app with optimized product listings and interactive features for an engaging shopping experience.   enables users to create projects, assign team members, and organize tasks on a Kanban-style board',
+    skills: ['angular 18, typescript,html,scss,Angular material,rxjs'],
+    link: 'https://task-manager-38a6d.web.app/',
     img: 'task-manager.png',
   },
   {
     name: 'fakeflix',
     description:
-      'Created a responsive streaming service interface clone in Angular, with real-time data fetching and smooth, user-friendly navigation.',
-      skills: ['angular 18, typescript,html,scss,Angular material,rxjs'],
+    'Built a task management application in Angular that        Developed an Angular-based e-commerDeveloped an Angular-based e-commerce app with optimized product listings and interactive features for an engaging shopping experience. Developed an Angular-based e-commerce app with optimized product listings and interactive features for an engaging shopping experience.ce app with optimized product listings and interactive features for an engaging shopping experience.   enables users to create projects, assign team members, and organize tasks on a Kanban-style board',
+    skills: ['angular 18, typescript,html,scss,Angular material,rxjs'],
     link: 'https://fakeflix-f0041.web.app/',
     img: 'fakeflix.png',
   },
