d6500d30b1860fdd3077f9f5d97691613651a0b3
Tamta <tamtakopadz16@gmail.com>
2024-11-29 21:56:58 +0400

    update project descriptions

--- src/app/core/data/project-data.ts
+++ src/app/core/data/project-data.ts
@@ -2,7 +2,7 @@ export const PROJECTS = [
   {
     name: 'ecommerce',
     description:
-      'Developed a fully functional e-commerce platform using Angular 17, integrating Firebase Realtime Database for backend management and Firebase Storage for product image hosting. The platform allows users to browse a variety of products, create personal accounts, add items to their wishlist, and complete the checkout process. Additionally, users can filter products based on categories, clothing color, and size, enhancing the shopping experience. With a focus on user experience, the platform offers intuitive navigation and seamless interactions. This project showcases my ability to integrate both frontend and backend technologies into a cohesive full-stack solution, providing a comprehensive and scalable e-commerce experience.',
+      'Built a fully functional e-commerce platform using Angular v17, integrating Firebase Realtime Database for backend management and Firebase Storage for product image hosting. The platform allows users to browse a variety of products, create personal accounts, add items to their wishlist, and complete the checkout process. Additionally, users can filter products based on categories, clothing color, and size, enhancing the shopping experience. With a focus on user experience, the platform offers intuitive navigation and seamless interactions. This project showcases my ability to integrate both frontend and backend technologies into a cohesive full-stack application, providing a comprehensive and scalable e-commerce solution.',
     skills: [
       'Angular 17',
       'TypeScript',
@@ -18,7 +18,7 @@ export const PROJECTS = [
   {
     name: 'task manager',
     description:
-      'Developed a Task Manager application using Angular 18 with a NestJS backend. The platform allows users to authenticate and create projects, where they can organize tasks within customizable kanban-style boards. Users can register tasks, assign them to different statuses using drag-and-drop functionality powered by Angular Material CDK, and manage task progress through dynamic columns. Additional features include the ability to create epics, define issue types, and invite team members to collaborate on projects. Users can also create new accounts and assign roles based on responsibilities, ensuring a role-based access structure. This project demonstrates my ability to integrate frontend and backend technologies to deliver a fully functional task management solution.',
+      'Developed a Task Manager application using Angular v18, integrating with a NestJS backend. The platform allows users to authenticate and create projects, where they can organize tasks within customizable kanban-style boards. Tasks can be moved across statuses using drag-and-drop functionality powered by Angular Material CDK, allowing seamless management of task progress through dynamic columns. Additional features include the ability to create epics, define issue types, and invite team members to collaborate on projects. Users can also create new accounts and assign roles based on responsibilities, ensuring a role-based access structure. This project highlights my expertise in building robust frontend solutions and integrating them with backend services for a fully functional task management platform.',
     skills: [
       'Angular 18',
       'TypeScript',
@@ -33,15 +33,8 @@ export const PROJECTS = [
   {
     name: 'fakeflix',
     description:
-      'Fakeflix is a modern movie browsing web app built with Angular 18, using Firebase for user authentication and integrating the TMDB API for real-time movie data.  The project offers a fully responsive layout optimized for various devices, featuring a dynamic movie carousel, interactive elements, and detailed movie overlays. Users can securely log in, browse movies, view detailed information, and log out. Real-time data from TMDB ensures up-to-date movie listings. The app also leverages RxJS for handling asynchronous operations and offers a seamless, engaging user experience with custom components, including a movie carousel and dynamic banner.',
-    skills: [
-      'Angular 18',
-      'TypeScript',
-      'HTML',
-      'SCSS',
-      'RxJS',
-      'Firebase',
-    ],
+"Fakeflix is a modern movie browsing web app built with Angular v18, featuring Firebase for user authentication and integration with the TMDB API for real-time movie data. The app delivers a fully responsive design, optimized for various devices, with engaging features like a dynamic movie carousel, interactive elements, and detailed movie overlays. Users can securely log in, explore a wide range of movies, access detailed information, and easily sign out. Real-time TMDB data ensures up-to-date movie listings, while custom components enhance the user experience, providing seamless navigation and a visually captivating interface." ,
+   skills: ['Angular 18', 'TypeScript', 'HTML', 'SCSS', 'RxJS', 'Firebase'],
     link: 'https://fakeflix-f0041.web.app/',
     img: 'fakeflix.png',
   },
